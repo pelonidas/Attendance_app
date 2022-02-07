eb2c06f20f44a334f480dc456416757ff05df841
pelonidas <janx803y@easv365.dk>
2022-02-07 18:04:34 +0100

    MainView init

---
+++ src/com/attendance/be/Student.java
@@ -0,0 +1,13 @@
+package com.attendance.be;
+
+import javafx.beans.property.StringProperty;
+
+public class Student {
+
+    private int id;
+    private StringProperty fName;
+    private StringProperty lName;
+    private StringProperty currentClass;
+
+
+}
