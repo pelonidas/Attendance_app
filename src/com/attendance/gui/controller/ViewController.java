9bfa3f691d6a0edf11d9970652bc71762e42fc79
pelonidas <janx803y@easv365.dk>
2022-02-07 17:20:57 +0100

    initial dir

---
+++ src/com/attendance/gui/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.attendance.gui.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
