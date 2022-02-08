647f20c5ac928b99621b1945d5aae29eee4a3390
pelonidas <janx803y@easv365.dk>
2022-02-08 10:32:03 +0100

    minor updates to check controller

--- src/com/attendance/gui/controller/CheckStudentAttendanceController.java
+++ src/com/attendance/gui/controller/CheckStudentAttendanceController.java
@@ -1,13 +1,30 @@
 package com.attendance.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CheckStudentAttendanceController implements Initializable {
+    @FXML
+    private Button cancelButton, confirmButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void handleCancelButton(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void handleConfirmButton(ActionEvent event) {
+        Stage stage = (Stage) confirmButton.getScene().getWindow();
+        stage.close();
     }
 }
