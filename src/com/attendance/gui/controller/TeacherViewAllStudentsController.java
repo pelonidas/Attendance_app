a31ccf2070ed574de3bd70934540ac883999f87c
Matej Kuka <mate1778@easv365.dk>
2022-02-23 21:23:44 +0100

    implemented combobox

--- src/com/attendance/gui/controller/TeacherViewAllStudentsController.java
+++ src/com/attendance/gui/controller/TeacherViewAllStudentsController.java
@@ -2,10 +2,14 @@ package com.attendance.gui.controller;
 
 import com.attendance.be.Student;
 import com.attendance.gui.model.MockData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -15,6 +19,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TeacherViewAllStudentsController implements Initializable {
+
+    private MockData model;
 
     @FXML
     private Button cancelBtn;
@@ -31,12 +37,16 @@ public class TeacherViewAllStudentsController implements Initializable {
     @FXML
     private TableView<Student> tableViewMiss;
 
-    private MockData model;
+    @FXML
+    private ComboBox<String> comboBox;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         model = new MockData();
+        //ObservableList<String> test = FXCollections.observableArrayList();
+        //test.addAll(model.getMonths());
+        comboBox.setItems(model.getMonths());
     }
 
     @FXML
